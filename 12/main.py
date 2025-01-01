00f4b6374558676382e992343f317f1847fb53a9
phipsi99 <schoppphilipp@googlemail.com>
2025-01-01 12:01:53 +0100

    12+13

--- 12/main.py
+++ 12/main.py
@@ -1,4 +1,37 @@
 from pathlib import Path
+import re
+
+def process(code):
+    registers = {"a": 0, "b": 0, "c": 1, "d": 0}
+    func_pointer = 0
+    while func_pointer < len(code):
+        instruction = code[func_pointer]
+        if "cpy" in instruction:
+            val = instruction.split(" ")[1]
+            reg = instruction.split(" ")[2]
+            if val.isnumeric():
+                registers[reg] = int(val)
+            else:
+                registers[reg] = registers[val]
+            func_pointer += 1
+        elif "inc" in instruction:
+            reg = instruction.split(" ")[1]
+            registers[reg] +=1
+            func_pointer += 1
+        elif "dec" in instruction:
+            reg = instruction.split(" ")[1]
+            registers[reg] -=1
+            func_pointer += 1
+        elif "jnz" in instruction:
+            cond = instruction.split(" ")[1]
+            val = instruction.split(" ")[2]
+            if cond == "1" or registers[cond] != 0:
+                func_pointer += int(val)
+            else:
+                func_pointer += 1
+    return registers
+
+
 
 def do_main(debug_mode=False):
     with open(Path('12/input.txt')) as file:
@@ -8,10 +41,7 @@ def do_main(debug_mode=False):
         with open(Path('12/test.txt')) as file:
             lines = [line.rstrip() for line in file]
 
-    point_sum = 0
-
-    for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+    print(process(lines))
 
 if __name__ == '__main__':
     do_main(False)
--- 13/main.py
+++ 13/main.py
@@ -1,17 +1,68 @@
+from collections import deque
 from pathlib import Path
 
+import numpy as np
+
+def create_maze(size, favourite_number):
+    maze = np.full((size, size), ".", str)
+    for x in range(size):
+        for y in range(size):
+            v = x*x + 3*x + 2*x*y + y + y*y
+            v += favourite_number
+            if bin(v).count("1") % 2 != 0:
+                maze[x,y] = "#"
+    return maze
+
+def solve_maze(maze, target):
+    queue = deque([((1,1), 0, [(1,1)])])
+    visited = set()
+    visited.add((1,1))
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    while queue:
+        pos, steps, path = queue.popleft()
+        if pos == target:
+            return steps, path
+        for dir in directions:
+            new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+            if 0 < new_pos[0] < len(maze[0]) and 0 < new_pos[1] < len(maze[0]) and new_pos not in visited:
+                if maze[new_pos] != "#":
+                    visited.add(new_pos)
+                    queue.append((new_pos, steps + 1, path + [new_pos]))
+    return 0, 0
+
+def count_locations(maze):
+    queue = deque([((1,1), 0, [(1,1)])])
+    visited = set()
+    visited.add((1,1))
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    while queue:
+        pos, steps, path = queue.popleft()
+        if steps == 50:
+            continue
+        for dir in directions:
+            new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+            if 0 <= new_pos[0] < len(maze[0]) and 0 <= new_pos[1] < len(maze[0]) and new_pos not in path:
+                if maze[new_pos] != "#":
+                    visited.add(new_pos)
+                    queue.append((new_pos, steps + 1, path + [new_pos]))
+    for vis in visited:
+        maze[vis] = 'O'
+    
+    return len(visited)
+
 def do_main(debug_mode=False):
-    with open(Path('13/input.txt')) as file:
-        lines = [line.rstrip() for line in file]
+    fav = 1362
+    target = (31, 39)
     
     if debug_mode:
-        with open(Path('13/test.txt')) as file:
-            lines = [line.rstrip() for line in file]
+        fav = 10
+        target = (7,4)
 
     point_sum = 0
 
-    for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+    maze = create_maze(1000, fav)
+    print(solve_maze(maze, target))
+    print(count_locations(maze))
 
 if __name__ == '__main__':
     do_main(False)
