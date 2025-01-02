3c3f37c00f6d8f0f0d0fd94074365a7b69ff2d0e
phipsi99 <schoppphilipp@googlemail.com>
2025-01-02 21:43:49 +0100

    19p1

--- 19/main.py
+++ 19/main.py
@@ -1,10 +1,8 @@
 
-def do_main(debug_mode=False):
-    no_elfs = 3018458
-    
-    if debug_mode:
-        no_elfs = 5
+import math
 
+
+def print_circle(no_elfs):
     elfs = [1] * no_elfs
     index = 0
     while elfs.count(no_elfs) != 1:
@@ -20,7 +18,21 @@ def do_main(debug_mode=False):
         index = (index +1) %no_elfs
 
     print(elfs)
-        
+
+def do_main(debug_mode=False):
+    no_elfs = 3018458
+    
+    if debug_mode:
+        no_elfs = 5
+
+    k = math.floor(math.log2(no_elfs))
+    p = 2 ** k
+    r = no_elfs - p
+    index = 2 * r
+    print(index+1)
+    
+
+    
     
 
 if __name__ == '__main__':
