f8e9741b19f8ea24071a1947b2e4c95ccef295aa
phipsi99 <schoppphilipp@googlemail.com>
2025-01-03 23:51:04 +0100

    add 20

--- 20/main.py
+++ 20/main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from tqdm import tqdm
 
 import numpy as np
 
@@ -12,13 +13,25 @@ def do_main(debug_mode=False):
 
     point_sum = 0
 
-    blocked_ips = np.zeros(3384842620)
+    ranges = []
+
 
     for line_index, line in enumerate(lines):
         start,end = [int(i) for i in line.split("-")]
-        blocked_ips[start:end] = 1
-    print(np.where(blocked_ips == 0)[0])
+        ranges.append((start, end))
+    ranges.sort()
 
+    new_ranges = []
+    for start, end in ranges:
+        if not new_ranges or new_ranges[-1][1] < start - 1:
+            new_ranges.append((start, end))
+        else:
+            new_ranges[-1] = (new_ranges[-1][0], max(new_ranges[-1][1], end))
+                
+    all_open = []
+    for index in tqdm(range(len(new_ranges)-1)):
+        all_open += list(range(new_ranges[index][1]+1, new_ranges[index+1][0]))
+    print(len(all_open) + 4294967295-new_ranges[-1][1] + new_ranges[0][0])
 
 if __name__ == '__main__':
-    do_main(True)
+    do_main(False)
--- 21/main.py
+++ 21/main.py
@@ -1,17 +1,74 @@
+from itertools import combinations
 from pathlib import Path
+from random import shuffle
+import re
+
+def unscramble(lines, password):
+    for line_index, line in enumerate(lines):
+        if "swap position" in line:
+            i1, i2 = [int(i) for i in re.findall(r"\d", line)]
+            temp = password[i1]
+            password[i1] = password[i2]
+            password[i2] = temp
+        elif "swap letter" in line:
+            i1, i2 = [password.index(i) for i in re.findall(r"\b[a-z]\b", line)]
+            temp = password[i1]
+            password[i1] = password[i2]
+            password[i2] = temp
+        elif "rotate based" in line:
+            steps = password.index(re.findall(r"\b[a-z]\b", line)[0])
+            if steps >= 4:
+                steps += 1
+            steps += 1
+            for i in range(steps):
+                p = password.pop()
+                password.insert(0, p)
+        elif "rotate" in line:
+            steps = int(re.findall(r"\d", line)[0])
+            if "left" in line:
+                for i in range(steps):
+                    p = password.pop(0)
+                    password.append(p)
+            else:
+                for i in range(steps):
+                    p = password.pop()
+                    password.insert(0, p)
+        elif "reverse" in line:
+            x,y = [int(i) for i in re.findall(r"\d", line)]
+            before = password[:x]
+            middle = password[x:y+1]
+            after = password[y+1:]
+            middle.reverse()
+            password = before + middle + after
+        elif "move" in line:
+            x,y = [int(i) for i in re.findall(r"\d", line)]
+            temp = password.pop(x)
+            password.insert(y, temp)
+    return password
 
 def do_main(debug_mode=False):
     with open(Path('21/input.txt')) as file:
         lines = [line.rstrip() for line in file]
+
+    password = list("abcdefgh")
     
     if debug_mode:
         with open(Path('21/test.txt')) as file:
             lines = [line.rstrip() for line in file]
+        password = list("abcde")
 
     point_sum = 0
 
-    for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+    print("".join(unscramble(lines, password)))
+
+    found_pw = ""
+    orig = list("abcdefgh")
+    while found_pw != "fbgdceah":
+        shuffle(orig)
+        found_pw = unscramble(lines, orig)
+    print(orig)
+
+                
 
 if __name__ == '__main__':
     do_main(False)
